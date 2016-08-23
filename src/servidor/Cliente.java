38e1612b42543af1cf9b53643755e68df3354633
B106 PC-11 <B106 PC-11@B106PC-11-PC>
2016-08-23 10:58:12 -0500

    ..

--- src/servidor/Cliente.java
+++ src/servidor/Cliente.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,30 +29,38 @@ public class Cliente {
         InputStream entrada = cliente.getInputStream();
         
         OutputStream salida= cliente.getOutputStream();
-          
         //Decoradores
         PrintStream escribir = new PrintStream(salida);
-        
         BufferedReader leer =  new BufferedReader(new InputStreamReader(entrada));
-        
-       
         BufferedReader teclado = new BufferedReader(new InputStreamReader (System.in));
         
-        System.out.println("Digite su universidad");
-        String enviar;
+        boolean flag=true;
+        while(flag){
+            System.out.println("Digite su universidad");
+            String uni;
+            uni = teclado.readLine();
+            System.out.println("Digite su Cargo");
+            String tipe;
+            tipe= teclado.readLine();
+            
+            String cadena = uni + tipe;
+            escribir.println(cadena);
+            
+            
+            String enviadoxServidor = leer.readLine();
+            System.out.println(enviadoxServidor);
+                 
+        if(uni.contains("cerrar")){
+            flag = false;
+          }
+        }
+            leer.close();
+            escribir.close();
+            entrada.close();
+            salida.close();
+            cliente.close();
         
-        enviar = teclado.readLine();
-        escribir.println(enviar);
-        String enviadoxServidor = leer.readLine();
-        System.out.println(enviadoxServidor);
        
-        
-        leer.close();
-        escribir.close();
-        entrada.close();
-        salida.close();
-        cliente.close();
-   
     }catch (Exception ex) {
                 Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
             }
